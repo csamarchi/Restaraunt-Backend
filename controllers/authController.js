4fd58b473bcb704e45d79c204b7f93a6f6698c35
Christine Samarchi <csamarchi@gmail.com>
2018-11-08 13:07:46 -0600

    created login and logout and register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,32 +1,68 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+router.post('/register', async (req,res,err) => {
+  console.log(req.session, ' this is session')
+try{
+  const password = req.body.password;
+  const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+  const userDbEntry = {};
+  userDbEntry.username = req.body.username;
+  userDbEntry.password = passwordHash;
+
+
+let createdUser= await User.create(userDbEntry,(err,createdUser) => {
+    req.session.logged = true;
+    req.session.username = req.body.username;
+    console.log(createdUser);
+    res.json({
+      status: 200,
+      data: 'register successful'
+    });
+  })
+}
+
+   catch(err){
+    console.log(err);
+    res.send(err);
+  }
+
+})
 
 router.post('/', async (req, res) => {
   console.log(req.body, ' this is session')
-
-
   try {
-
     const user = await User.create(req.body);
-
     req.session.logged = true;
     req.session.username = req.body.username;
-
-
     res.json({
       status: 200,
       data: 'login successful'
     });
-
-
-
   } catch(err){
     console.log(err);
     res.send(err);
   }
 });
 
+router.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      res.json({
+      status: 404,
+      data: 'log out unsuccessful',
+    });
+    } else {
+      res.json({
+      status: 200,
+      data: 'Logout successful'
+     })
+   }
+ })
+})
 
 
 
